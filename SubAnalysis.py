bea9c3f6b14e03c36112d3ced07606e4f532f4e6
Liam <liamdevlin2305@gmail.com>
2019-08-09 17:21:08 +0100

    Update SubAnalysis.py

--- SubAnalysis.py
+++ SubAnalysis.py
@@ -6,3 +6,20 @@ from matplotlib import rcParams
 import statistics
 
 sub_analysis_path = "res/analysis_data/subjective_analysis.xml"
+
+sub_raw = parseAnalysisData(sub_analysis_path)
+sub_data = sub_raw["Test"]["Participant"]
+
+
+enjoyments = []
+replays = []
+complexities = []
+
+skills = []
+instruments = []
+listen_times = []
+
+for participant in sub_data:
+    participant_no = eval(participant["participantId"])
+    print("Participant %d" % participant_no)
+    
